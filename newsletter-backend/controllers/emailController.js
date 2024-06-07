83baf801b6b5852bab543bf2082e890068e546bf
José Daniel <jose.daniell@outlook.com>
2024-06-07 16:47:01 -0300

    added newsletter jobs

--- newsletter-backend/controllers/emailController.js
+++ newsletter-backend/controllers/emailController.js
@@ -1,4 +1,6 @@
 import { getEmails, insertEmail } from '../repository/emailRepository.js'
+import { Resend } from 'resend';
+
 
 const EmailController = {
     getEmails: async (req, res) => {
@@ -43,6 +45,37 @@ const EmailController = {
         } catch (error) {
             res.status(500)
             res.json({ message: 'Erro ao cadastrar email' })
+            return
+        }
+    },
+    sendEmail: async (req, res) => {
+        const { email, subject, message } = req.body;
+
+        if (!email || !subject || !message) {
+            return res.status(400).json({ message: 'Email, subject and message are required' });
+        }
+
+
+        try {
+
+            const emails = await getEmails()
+
+            const emailsAsStrings = emails.map((email) => email.email)
+
+            const resend = new Resend('re_S4vsLutT_2RxvtXU7yPFcyL4QKYimv1JB');
+
+            await resend.emails.send({
+                from: 'onboarding@resend.dev',
+                to: "danieldigitalcollege@gmail.com",
+                subject: subject,
+                html: `<p>${message}!</p>`
+            });
+
+            res.json({ message: 'Email enviado com sucesso!' })
+            return
+        } catch (error) {
+            res.status(500)
+            res.json({ message: 'Erro ao enviar email' + error.message })
             return
         }
     }
---
+++ newsletter-jobs/index.js
@@ -0,0 +1,28 @@
+import cron from "node-cron"
+import moment from "moment-timezone";
+import { Resend } from "resend";
+
+
+cron.schedule('45 16 * * *', async () => {
+    try {
+        const saoPauloTime = moment.tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss');
+
+        console.info(`Running cron job at ${saoPauloTime}`);
+    
+        const resend = new Resend('re_S4vsLutT_2RxvtXU7yPFcyL4QKYimv1JB');
+    
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: "danieldigitalcollege@gmail.com",
+            subject: "subject",
+            html: `<p>oioioioioioio</p>`
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}, {
+    scheduled: true,
+    timezone: "America/Sao_Paulo"
+});
+
+console.log('Cron job set up to run every day at 12:00 PM São Paulo time.');
